0288457bf982972dde781ab6ec368d6143bcf214
Santi1718 <santiago.max.qui@gmail.com>
2025-08-13 06:32:48 -0500

    feat: :zap: update Dia6

---
+++ Dia6/LSP/index.js
@@ -0,0 +1,6 @@
+/*
+El Liskov Substitución Principle (LSP) establece que los objetos de una subclase deben poder reemplazar a los de su clase
+base sin afectar el comportamiento del sistema. En otras palabras, las subclases deben ser capaces de mantener las propiedades
+y comportamientos de la clase padre.
+*/
+
---
+++ Dia6/index.js
@@ -0,0 +1,8 @@
+/*
+¿ Qué son los principios S.O.L.I.D?
+Son principios de diseño, los cuales estan orientados a realizar
+codigo mantenible, escalable y limpio.
+Para ello hay 5 principios:
+S: Single Responsibility Principle (SRP)
+O: Open/Close Pri
+*/
